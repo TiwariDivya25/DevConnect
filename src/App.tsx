27431be8f45f218babbddee71c10094c4001b720
ubanand36 <ak0591640@gmail.com>
2026-01-03 13:21:59 +0530

    Added footer in the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,39 @@
-import { Routes, Route } from 'react-router'
-import Home from './pages/Home.tsx'
-import CreatePostPage from './pages/CreatePostPage.tsx'
-import Navbar from './components/Navbar.tsx'
-import PostPage from './pages/PostPage.tsx'
-import CreateCommunityPage from './pages/CreateCommunityPage.tsx'
-import {CommunityPage} from './pages/CommunityPage.tsx'
-import { CommunitiesPage } from './pages/CommunitiesPage.tsx'
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import CreatePostPage from "./pages/CreatePostPage";
+import PostPage from "./pages/PostPage";
+import CreateCommunityPage from "./pages/CreateCommunityPage";
+import { CommunityPage } from "./pages/CommunityPage";
+import { CommunitiesPage } from "./pages/CommunitiesPage";
 
 function App() {
   return (
     <>
-      <div>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/create" element={<CreatePostPage />} />
-            <Route path="/post/:id" element={<PostPage />} />
-            <Route path="/communities/create" element={<CreateCommunityPage />} />
-            <Route path="/communities" element={<CommunitiesPage />} />
-            <Route path="/communities/:id" element={<CommunityPage />} />
-          </Routes>
-        </div>
-      </div>
+      <Navbar />
+
+      <main style={{ minHeight: "80vh" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePostPage />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/communities" element={<CommunitiesPage />} />
+          <Route
+            path="/communities/create"
+            element={<CreateCommunityPage />}
+          />
+          <Route path="/communities/:id" element={<CommunityPage />} />
+        </Routes>
+      </main>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2026 YourWebsiteName. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
