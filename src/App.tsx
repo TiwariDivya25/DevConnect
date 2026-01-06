72d8c89920bf9a7389cc427a2de403089f223197
shivam154co <shivampawar44330j@gmail.com>
2026-01-06 12:11:46 +0530

    Add custom 404 page (#33)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import CreatePostPage from './pages/CreatePostPage.tsx'
 import Navbar from './components/Navbar.tsx'
@@ -7,23 +7,28 @@ import CreateCommunityPage from './pages/CreateCommunityPage.tsx'
 import {CommunityPage} from './pages/CommunityPage.tsx'
 import { CommunitiesPage } from './pages/CommunitiesPage.tsx'
 import MessagesPage from './pages/MessagesPage.tsx'
+import NotFound from './components/NotFound.tsx'  // Import the 404 page
 
 function App() {
   return (
     <>
       <div>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/create" element={<CreatePostPage />} />
-            <Route path="/post/:id" element={<PostPage />} />
-            <Route path="/communities/create" element={<CreateCommunityPage />} />
-            <Route path="/communities" element={<CommunitiesPage />} />
-            <Route path="/communities/:id" element={<CommunityPage />} />
-            <Route path="/messages" element={<MessagesPage />} />
-          </Routes>
-        </div>
+        <Router>  {/* Wrap everything with Router */}
+          <Navbar />
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/create" element={<CreatePostPage />} />
+              <Route path="/post/:id" element={<PostPage />} />
+              <Route path="/communities/create" element={<CreateCommunityPage />} />
+              <Route path="/communities" element={<CommunitiesPage />} />
+              <Route path="/communities/:id" element={<CommunityPage />} />
+              <Route path="/messages" element={<MessagesPage />} />
+              {/* Add 404 route at the end - catches all undefined routes */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </div>
     </>
   )
